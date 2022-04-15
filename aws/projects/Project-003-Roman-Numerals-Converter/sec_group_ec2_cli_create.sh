287f12f9890c4d6e9fa9ca2120327215ba7fd911
gulcin <cankilicgulcin@gmail.com>
2022-04-14 20:46:34 -0400

    all projects update

--- aws/projects/Project-003-Roman-Numerals-Converter/sec_group_ec2_cli_create.sh
+++ aws/projects/Project-003-Roman-Numerals-Converter/sec_group_ec2_cli_create.sh
@@ -69,9 +69,9 @@ yum update -y
 yum install python3 -y
 pip3 install flask
 cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/xxxxxxxx/my-projects-2/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/index.html
-wget -P templates https://raw.githubusercontent.com/xxxxxxxx/my-projects-2/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/result.html
-wget https://raw.githubusercontent.com/xxxxxxxx/my-projects-2/main/aws/projects/Project-001-Roman-Numerals-Converter/app.py
+wget -P templates https://raw.githubusercontent.com/xxxxxxxx/my_projects-2/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/index.html
+wget -P templates https://raw.githubusercontent.com/xxxxxxxx/my_projects-2/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/result.html
+wget https://raw.githubusercontent.com/xxxxxxxx/my_projects-2/main/aws/projects/Project-001-Roman-Numerals-Converter/app.py
 python3 app.py
 
 
